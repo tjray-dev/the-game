4eb1e4ead84f49290dcf06b884c36fb72402bfbb
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2020-06-01 16:34:22 -0600

    Add files via upload

---
+++ engine.rb
@@ -0,0 +1,12 @@
+class Engine
+	def map()
+		#creates a new instance of the dungeon class
+		#prints the contents of the room methods
+		#rolls a random number to check for a fight
+	end
+
+	def fight()
+		#create a monster
+		#display monster stats
+		#combat loop until player || enemy hp == 0
+	end      
