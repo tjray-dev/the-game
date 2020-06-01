f526ac02f2755693cc59c81fb1110e54991ef5ff
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2020-06-01 16:05:31 -0600

    Add files via upload

--- character.rb
+++ character.rb
@@ -1,36 +1,63 @@
 class Character
+	
+	def creation()
+		race["elf", "human", "dwarf"]
+		lifepath ["guard", "soldier", "knight"]
+		wpn["sword", "axe", "spear"]
+		armr["light", "medium", "heavy"]
 
-	#hero[hero_name, str, dex, int, ac, dmg]
-	race[
-		"elf" => "str", "dex", "int"
-		"human" => "str", "dex", "int"
-		"dwarf" => "str", "dex", "int"
-		]
-	lifepath[
-			"Guard" => "wpn", "armr"
-			"Ranger" => "wpn", "armr"
-			"knight" => "wpn", "armr"
-			]
-def creation( hero_name, hero_race, hero_lifepath)
-#this walks a player through creating a hero
-	puts "What is your hero's name"
-	hero_name = $stdin.gets.chomp
+		puts "what is your name hero?"
+		hero_name = $stdin.gets.chomp
 
-	puts "Choose your race."
-	print race[]
-	hero_race = $stdin.gets.chomp
-	#if/else loop for errors  
+		puts "Welcome #{hero_name}, what is you race?"
+		print race
+		hero_race = $stdin.gets.chomp
+		if hero_race == "elf"
+			#set stats
+		elsif hero_race == "human"
+			#set stats
+		elsif hero_race == "dwarf"
+			#set stats
+		else 
+			#error
+		end
 
-	puts "Choose your lifepath"
-	print lifepath[]
-	hero_lifepath = $stdin.gets.chomp
-	#if/else loop for errors
-#save these variables as a list for use later
-	hero[hero_name, hero_race, hero_lifepath, str, dex, int, wpn, armr,]
-end
+		puts "Great #{hero_name} of the #{hero_race} choose your weapon."
+		print wpn[]
+		hero_wpn = $stdin.gets.chomp
+		if hero_wpn == "sword" || "axe" || "spear"
+			#set her_wpn	
+		else
+			#error
+		end
 
-def npc()
-	#this makes random rolls to create npcs and enemies 
-end
+		puts "Great #{hero_name}, what was your life path"
+		print lifepath[]
+		hero_lifepath = $stdin.gets.chomp
+		if hero_lifepath == "guard" || "soilder" || "knight"
+			#set hero_lifepath
+		else
+			#error
+		end
 
+		puts "Great #{hero_name}, choose your armor."
+		print armr[]
+		hero_armr = $stdin.gets.chomp
+		if hero_armr == "light" || "medium" || "heavy"
+			#set hero_armr
+		else
+			#error
+		end
 
+		hero[hero_name, hero_race, hero_lifepath, hero_wpn, hero_armr]
+	end
+
+	def monster()
+		race["goblin", "orc", "kobold"]
+		wpn["sword", "axe", "spear"]
+		armr["light", "medium", "heavy"]
+
+		#code for rolling a random monster
+
+		monster[monster_race, monster_wpn, monster_armr]
+	end
---
+++ dungeon.rb
@@ -0,0 +1,16 @@
+class Dungeon
+	def entry()
+		#this is the entry room 
+		#this is where the player starts
+	end
+
+	def room_1()
+	end
+
+	def room_2()
+	end
+
+	def treasure_room()
+		#this is the treasure room
+		#good job you won
+	end
